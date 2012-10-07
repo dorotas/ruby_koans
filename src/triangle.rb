5f3d62718b72d79ab677ee1784226a9c74b3df6c
Dorota <d.suchocka@gmail.com>
2012-10-07 17:44:22 +0200

    triangle with else in case

--- src/triangle.rb
+++ src/triangle.rb
@@ -17,7 +17,7 @@ def triangle(a, b, c)
   case [a,b,c].uniq.size
     when 1 then :equilateral
     when 2 then :isosceles
-    when 3 then :scalene
+    else        :scalene
   end
 
   a, b, c = [a, b, c].sort
